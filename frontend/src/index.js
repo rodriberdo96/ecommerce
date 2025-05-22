8f92905bbb42cad0fad7fbb5e3869e438edab642
Rodri Berdomas <rodriberdomas@gmail.com>
2025-05-22 13:06:12 -0300

    .

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
-import ShopContextProvider from './context/shopcontext.js';
+import ShopContextProvider from '../../frontend/src/context/shopcontext.jsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ShopContextProvider >
