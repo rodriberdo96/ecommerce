51d34843486c8f78714b99c1787dc935a62260f5
Rodri Berdomas <rodriberdomas@gmail.com>
2025-05-22 20:32:24 -0300

    .

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,10 @@ app.use(express.json());
 
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch(err => console.error('Error al conectar a MongoDB:', err));
+
 
 
 //API Creation
